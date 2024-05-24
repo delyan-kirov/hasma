4bd54b48b16464dca2abc158224d4540f2ba5365
delyan-kirov <delyan.k.kirov@gmail.com>
2024-05-24 21:33:04 +0300

    Small refactoring of the tokenizer

--- smallang/lexematizer.cpp
+++ smallang/lexematizer.cpp
@@ -28,35 +28,117 @@ struct Lexema {
   Position position;
 };
 
-void lexematize(const std::string &source) {
+void skipWhitespace(const std::string &source, size_t &position) {
+  while (position < source.size() && std::isspace(source[position])) {
+    ++position;
+  }
+}
+
+void handleEndOfLine(size_t &currentLine, size_t &linePosition) {
+  ++currentLine;
+  linePosition = 0;
+}
+
+void processText(const std::string &source, size_t &position,
+                 size_t &linePosition, size_t currentLine,
+                 std::vector<Lexema> &lexemes) {
+  std::string text;
+  ++position;
+  ++linePosition;
+
+  while (position < source.size() && source[position] != '"') {
+    text += source[position];
+    ++position;
+    ++linePosition;
+  }
+
+  if (position >= source.size() || source[position] != '"') {
+    std::cerr << "ERROR: Did not find the closing \"\n";
+    exit(1);
+  }
+
+  text += '"';
+  ++position;
+  ++linePosition;
+
+  lexemes.push_back(
+      Lexema{LexemaType::TEXT, text, Position{currentLine, linePosition}});
+}
+
+void processNumber(const std::string &source, size_t &position,
+                   size_t &linePosition, size_t currentLine,
+                   std::vector<Lexema> &lexemes) {
+  std::string number;
+
+  while (position < source.size() && std::isdigit(source[position])) {
+    number += source[position];
+    ++position;
+    ++linePosition;
+  }
+
+  lexemes.push_back(
+      Lexema{LexemaType::NUMBER, number, Position{currentLine, linePosition}});
+}
+
+void processVariable(const std::string &source, size_t &position,
+                     size_t &linePosition, size_t currentLine,
+                     std::vector<Lexema> &lexemes) {
+  std::string variable;
+  variable += source[position];
+  ++position;
+  ++linePosition;
+
+  while (position < source.size() &&
+         (std::isalnum(source[position]) || source[position] == '_')) {
+    variable += source[position];
+    ++position;
+    ++linePosition;
+  }
+
+  lexemes.push_back(
+      Lexema{LexemaType::ALIAS, variable, Position{currentLine, linePosition}});
+}
+
+void handleError(char currentChar) {
+  std::cerr << "\nERROR: Did not recognize lexeme: " << currentChar << "\n";
+  exit(1);
+}
+
+void printLexemes(std::vector<Lexema> &lexemes) {
+  // For demonstration, print the collected lexemes
+  for (const auto &lex : lexemes) {
+    std::cout << "\nLexeme: " << lex.lexeme << " at line " << lex.position.line
+              << ", column " << lex.position.column;
+  }
+}
+
+std::vector<Lexema> lexematize(const std::string &source) {
   size_t currentLine = 0;
   size_t position = 0;
   size_t linePosition = 0;
 
   std::vector<Lexema> lexemes;
 
-  while (position < source.size()) {
+  while (position < source.size() - 1) {
+    skipWhitespace(source, position);
+
     char currentChar = source.at(position);
 
     switch (currentChar) {
-    case ' ':
-      break;
     case '\n':
-      ++currentLine;
-      linePosition = 0;
+      handleEndOfLine(currentLine, linePosition);
       break;
     case ';':
-      lexemes.push_back(Lexema{LexemaType::DIVIDOR, ";",
-                               Position{currentLine, linePosition}});
+      lexemes.push_back(
+          Lexema{LexemaType::DIVIDOR, ";", {currentLine, linePosition}});
       break;
     case '/':
       if (source.at(position + 1) == '/') {
         ++position;
-        ++linePosition;
         while (source.at(position) != '\n') {
           ++position;
-          ++linePosition;
         }
+        linePosition = 0;
       } else {
         std::cerr
             << "\nERROR: the char / is not recognized outside of // for now\n";
@@ -65,77 +147,26 @@ void lexematize(const std::string &source) {
       break;
     case '=':
       lexemes.push_back(
-          Lexema{LexemaType::EQUAL, "=", Position{currentLine, linePosition}});
+          Lexema{LexemaType::EQUAL, "=", {currentLine, linePosition}});
       break;
     case '(':
-      lexemes.push_back(Lexema{LexemaType::PAREN_LEFT, "(",
-                               Position{currentLine, linePosition}});
+      lexemes.push_back(
+          Lexema{LexemaType::PAREN_LEFT, "(", {currentLine, linePosition}});
       break;
     case ')':
-      lexemes.push_back(Lexema{LexemaType::PAREN_RIGHT, ")",
-                               Position{currentLine, linePosition}});
-      break;
-    case '"': {
-      std::string text(1, currentChar);
-      ++position;
-      ++linePosition;
-
-      while (source.at(position) != '"') {
-        text += source.at(position);
-        ++position;
-        ++linePosition;
-        if (position > source.size()) {
-          std::cerr << "ERROR: Did not find the closing \"\n";
-          exit(1);
-        }
-      }
-
-      if (position < source.size()) {
-        text += '"';
-        ++position;
-        ++linePosition;
-      }
-
       lexemes.push_back(
-          Lexema{LexemaType::TEXT, text, Position{currentLine, linePosition}});
+          Lexema{LexemaType::PAREN_RIGHT, ")", {currentLine, linePosition}});
       break;
-    }
+    case '"':
+      processText(source, position, linePosition, currentLine, lexemes);
+      break;
     default:
-      if (isdigit(source.at(position))) {
-        std::string number;
-
-        while (position < source.size() && isdigit(source.at(position))) {
-          number += source.at(position);
-          ++position;
-        }
-
-        lexemes.push_back(Lexema{LexemaType::NUMBER, number,
-                                 Position{currentLine, linePosition}});
-        --position;
-      } else if (isalpha(source.at(position))) {
-        std::string variable(1, currentChar);
-        ++position;
-        if (source.at(position) == ')') {
-          --position;
-          lexemes.push_back(Lexema{LexemaType::ALIAS, variable,
-                                   Position{currentLine, linePosition}});
-        } else {
-
-          while (position < source.size() &&
-                 (isalnum(source.at(position)) || source.at(position) == '_')) {
-
-            variable += source.at(position);
-            ++position;
-            ++linePosition;
-          }
-
-          lexemes.push_back(Lexema{LexemaType::ALIAS, variable,
-                                   Position{currentLine, linePosition}});
-        }
+      if (isdigit(currentChar)) {
+        processNumber(source, position, linePosition, currentLine, lexemes);
+      } else if (isalpha(currentChar)) {
+        processVariable(source, position, linePosition, currentLine, lexemes);
       } else {
-        std::cerr << "\nERROR: Did not recognize lexeme: " << currentChar
-                  << "\n";
-        exit(1);
+        handleError(currentChar);
       }
       break;
     }
@@ -144,11 +175,9 @@ void lexematize(const std::string &source) {
     ++linePosition;
   }
 
-  // For demonstration, print the collected lexemes
-  for (const auto &lex : lexemes) {
-    std::cout << "\nLexeme: " << lex.lexeme << " at line " << lex.position.line
-              << ", column " << lex.position.column;
-  }
+  printLexemes(lexemes);
+
+  return lexemes;
 }
 
 const static std::string source = "a = 2\n"
@@ -184,3 +213,6 @@ int main() {
   }
   return 0;
 }
+
+// TODO Fix ;
+// TODO Fix )
