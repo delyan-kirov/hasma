20bfa5e5915cfe2775f9739a6019dec6ea0e6b5d
delyan-kirov <delyan.k.kirov@gmail.com>
2024-09-23 10:37:14 +0300

    [injection] Tokenizing subset of Hasma

--- src/main.rs
+++ src/main.rs
@@ -2,65 +2,125 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-enum Variable {
-    Txt(String),
-    Integer(i64),
-    Natural(i64),
+#[derive(std::fmt::Debug)]
+enum Type {
+    Int,
+    Txt,
+    Real,
+    Bool,
     Unit,
-    Tuple(Box<Variable>, Box<Variable>),
-    Record(Vec<Variable>, HasmaTypes),
-    Enumeration(Vec<Expression>),
-    Boolean(bool),
+    IO(Box<Type>),
+    Fn(Box<Type>, Box<Type>),
+    Any,
 }
 
-enum Expression {
-    Variable(Variable, HasmaTypes),
-    Abstraction {
-        parameter: HasmaTypes,
-        definition: Box<Expression>,
-    },
-    Application {
-        hasma_type: HasmaTypes,
-        parameter: Box<Expression>,
-        definition: Box<Expression>,
-    }, // function + argument
-    DoBlock(Vec<Expression>),  // do block
-    LetBlock(Vec<Expression>), // let block
-    IfBlock(Vec<(Expression, Expression)>), // if block
-                               // case block
+#[derive(std::fmt::Debug)]
+enum Literal {
+    Int(i32),
+    Txt(String),
+    Real(f64),
+    Bool(bool),
+    Unit,
 }
 
-enum HasmaTypes {
-    Boolean,                                    // Boolean data type
-    Txt,                                        // Utf8 string
-    Function(Box<HasmaTypes>, Box<Expression>), // function variable + function body
-    Int,                                        // signed integer
-    Nat,                                        // unsigned integer
-    Unit,                                       // trivial type
-    Tuple(Box<HasmaTypes>, Box<HasmaTypes>),    // tuple type
-    Record(Vec<HasmaTypes>),                    // record type
-    Enumeration(Vec<(String, HasmaTypes)>),     // enum tag + type
-    NoneDetermined,                             // type either undetemined or wrong
+#[derive(std::fmt::Debug)]
+enum Binding {
+    Name(String),
+    Type(Type),
 }
 
-fn parser<T>(file: T, definitions: Vec<Expression>) -> Expression
+#[derive(std::fmt::Debug)]
+enum Closure {
+    Param(Binding),
+    Def(Box<Expr>),
+    Type(Type),
+}
+
+#[derive(std::fmt::Debug)]
+enum Expr {
+    Var(Binding),
+    Literal(Literal),
+    Closure(Closure),
+    App(Box<Expr>, Box<Expr>),
+}
+
+#[derive(std::fmt::Debug)]
+enum Token {
+    Int(i32),
+    Name(String),
+    Real(f64),
+    Equal,
+    FnArrow,
+    ParenL,
+    ParenR,
+    Colon,
+    SemiColon,
+    FnSlash,
+}
+
+fn tokenize<It>(input: It) -> Vec<Token>
 where
-    T: Iterator<Item = char>,
+    It: Iterator<Item = char>,
 {
-    let mut expr: Expression;
-    let mut expr_string: String = String::new();
+    let mut tokens = Vec::<Token>::new();
+    let mut curr = String::new();
+    let mut line: usize = 0;
 
-    for c in file {
+    let tokenize_string = |s: &mut String, tokens: &mut Vec<Token>| {
+        if let Ok(number) = s.parse::<i32>() {
+            tokens.push(Token::Int(number));
+        } else if let Ok(real) = s.parse::<f64>() {
+            tokens.push(Token::Real(real));
+        } else if !s.is_empty() {
+            tokens.push(Token::Name(s.clone()));
+        }
+
+        *s = "".to_string();
+    };
+
+    for c in input {
         match c {
-            ' ' | '\n' => {
-                println!("{}", expr_string);
-                expr_string = "".to_string();
+            '\n' => line += 1,
+            t if t.is_whitespace() => tokenize_string(&mut curr, &mut tokens),
+            ';' => {
+                tokenize_string(&mut curr, &mut tokens);
+                tokens.push(Token::SemiColon)
             }
-            _ => {
-                expr_string += &c.to_string();
+            ':' => {
+                tokenize_string(&mut curr, &mut tokens);
+                tokens.push(Token::Colon)
             }
+            '(' => {
+                tokenize_string(&mut curr, &mut tokens);
+                tokens.push(Token::ParenL)
+            }
+            ')' => {
+                tokenize_string(&mut curr, &mut tokens);
+                tokens.push(Token::ParenR)
+            }
+            '=' => {
+                tokenize_string(&mut curr, &mut tokens);
+                tokens.push(Token::Equal)
+            }
+            '>' => {
+                if &curr[0..] == "-" {
+                    tokens.push(Token::FnArrow);
+                    curr = "".to_string();
+                } else {
+                    tokenize_string(&mut curr, &mut tokens);
+                    eprintln!("WARNING {line}: use of '-' and '>' as anything but the function constructor '->' is not supported yet");
+                }
+            }
+            '\\' => tokens.push(Token::FnSlash),
+            t if t.is_alphanumeric() || t == '_' || t == '-' || t == '-' => curr.push(t),
+            t => eprintln!("WARNING {line}: character: {t} not supported, ignoring it."),
         }
     }
+
+    tokens
+}
+
+fn parse(tokens: Vec<Token>) -> Vec<Expr> {
     todo!()
 }
 
@@ -73,8 +133,11 @@ fn main() -> std::io::Result<()> {
             .map(|b| b as char)
     };
 
-    let definitions: Vec<Expression> = Vec::new();
-    let _ = parser(file, definitions);
+    let tokens = tokenize(file);
+
+    for t in tokens {
+        println!("{:?}", t);
+    }
 
     Ok(())
 }
